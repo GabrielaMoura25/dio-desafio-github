49bb73ad769d41c7326980fae5d8385716a128ea
Gabriela Moura <gabrieladaniel49@hotmail.com>
2022-12-07 23:39:00 -0300

    :sparkles: revisão e mini desafio de variaveis e operadores

---
+++ Primeiros-passos-com-JavaScript/Variaveis-e-operadores/aula.js
@@ -0,0 +1,33 @@
+//usar o: node <nome do arquivo>, para imprimir o resultado no terminal
+
+//01 - variáveis let e const(armazenar espaços na memória do computador):
+let variavel = 10; // let é uma variável que pode ser alterada depois de declarada
+
+// console.log(variavel);
+
+variavel = 20;
+
+// console.log(variavel);
+
+const pi = 3.14; // const não pode ser alterada depois de declarada
+
+// console.log(pi);
+
+// pi = 20;
+
+// console.log(pi); // error: Assignment to constant variable.
+
+//02 - atribuição de valores:
+
+let vale = 5; // ' = ' um símbolo de igual é uma atribuição
+
+//03 - operadores:
+
+let soma = 10 + 10; // operador de soma
+// console.log(soma);
+let subtracao = 10 - 10; // operador de subtração
+// console.log(subtracao);
+let multiplicacao = 10 * 10; // operador de multiplicação
+// console.log(multiplicacao);
+let divisao = 10 / 10; // operador de divisão
+// console.log(divisao);
